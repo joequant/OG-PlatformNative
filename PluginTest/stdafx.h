d24df83f6c0b6a04377f198f7939a2aab87300ce
Andrew Griffin <andrew@opengamma.com>
2011-03-08 18:51:36 +0000

    Repository created.

---
+++ PluginTest/stdafx.h
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+#ifndef __inc_og_pirate_plugin_stdafx_h
+#define __inc_og_pirate_plugin_stdafx_h
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <tchar.h>
+#include <strsafe.h>
+#ifdef __cplusplus
+#pragma warning(disable:4995)
+#endif /* ifdef __cplusplus */
+#else /* ifdef _WIN32 */
+#include <stdio.h>
+#include <stdlib.h>
+#endif /* ifdef _WIN32 */
+
+#include <assert.h>
+
+#include <Util/AbstractTest.h>
+
+#endif /* ifndef __inc_og_pirate_plugin_stdafx_h */
