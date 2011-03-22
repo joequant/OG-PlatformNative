4d9f41d9669e8b317eeef75808188b59e4acc954
Andrew Griffin <andrew@opengamma.com>
2011-03-22 12:19:49 +0000

    Placeholder test for clover to run.

---
+++ Ext/tests/unit/com/opengamma/rstats/TodoTest.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+package com.opengamma.rstats;
+
+import org.junit.Test;
+
+/**
+ * Placeholder for compilation to work.
+ */
+public class TodoTest {
+
+  @Test
+  public void todo () {
+  }
+
+}
