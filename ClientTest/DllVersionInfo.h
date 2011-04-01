97a08f1e8a7f76e4d88af40b478bae8093f41b1d
Andrew Griffin <andrew@opengamma.com>
2011-04-01 14:48:59 +0100

    Placeholders for resource info (Win32 build)

---
+++ ClientTest/DllVersionInfo.h
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+
+#ifndef __inc_og_pirate_clienttest_dllversioninfo_h
+#define __inc_og_pirate_clienttest_dllversioninfo_h
+
+#define DllVersion_FileDescription	"R integration client test"
+#ifdef _WIN32
+#define DllVersion_OriginalFilename	"ClientTest.dll"
+#else
+#define DllVersion_OriginalFilename "ClientTest"
+#endif
+
+// TODO: need product name ??
+
+#endif /* ifndef __inc_og_pirate_clienttest_dllversioninfo_h */
