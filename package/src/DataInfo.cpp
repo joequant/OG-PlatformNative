2aa002617de4dc1bbe68db4ee9c030c4143a592a
Andrew Griffin <andrew@opengamma.com>
2011-10-04 13:32:24 +0100

    Documented the CDataInfo class.

--- package/src/DataInfo.cpp
+++ package/src/DataInfo.cpp
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
  *
  * Please see distribution for license.
@@ -11,6 +11,14 @@
 
 LOGGING (com.opengamma.rstats.package.DataInfo);
 
+/// Applies the content of a DataInfo metadata message to the SEXP value. If the metadata contains a wrapper
+/// class name, the interop function declared on that class is used to convert the value from its R/Java
+/// encoding to a rich R object.
+///
+/// @param[in] poR callback service for invoking R (defined on the original caller's environment)
+/// @param[in] rawResult the original result to process
+/// @param[in] pInfo metadata to apply, never NULL
+/// @return the new result if there was an interop conversion, otherwise the original result object
 SEXP CDataInfo::Apply (const CRCallback *poR, SEXP rawResult, const com_opengamma_rstats_msg_DataInfo *pInfo) {
 	int nUnprotect = 0;
 	if (pInfo->_wrapperClass) {
--- package/src/DataInfo.h
+++ package/src/DataInfo.h
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 2011 - present by OpenGamma Inc. and the OpenGamma group of companies
  *
  * Please see distribution for license.
@@ -10,13 +10,24 @@
 #include "com_opengamma_rstats_msg_DataInfo.h"
 #include "RCallback.h"
 
+/// Helper functions for converting the OG-Language DataInfo construct to/from the R SEXP structure.
 class CDataInfo {
 private:
+
+	/// Prevents instantiation.
 	CDataInfo () { }
+
+	/// Prevents instantiation.
 	~CDataInfo () { }
+
 public:
-	static SEXP Apply (const CRCallback *poR, SEXP rawResult, const com_opengamma_rstats_msg_DataInfo *pInfo);
+
+	/// Release the memory allocated for a DataInfo.
+	///
+	/// @param[in] pInfo structure to release
 	static void Release (com_opengamma_rstats_msg_DataInfo *pInfo) { com_opengamma_rstats_msg_DataInfo_free (pInfo); }
+
+	static SEXP Apply (const CRCallback *poR, SEXP rawResult, const com_opengamma_rstats_msg_DataInfo *pInfo);
 };
 
 #endif /* ifndef __inc_og_rstats_package_datainfo_h */
