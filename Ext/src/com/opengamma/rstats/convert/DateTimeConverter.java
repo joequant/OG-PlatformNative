aa22323aa688097dffbf3dabc8addc55231784a9
Andrew Griffin <andrew@opengamma.com>
2011-10-05 12:05:02 +0100

    Convert between POSIXct double and Instant

--- Ext/src/com/opengamma/rstats/convert/DateTimeConverter.java
+++ Ext/src/com/opengamma/rstats/convert/DateTimeConverter.java
@@ -5,39 +5,54 @@
  */
 package com.opengamma.rstats.convert;
 
+import static com.opengamma.language.convert.TypeMap.MINOR_LOSS;
 import static com.opengamma.language.convert.TypeMap.ZERO_LOSS;
 
 import java.util.Map;
 
+import javax.time.Instant;
 import javax.time.calendar.LocalDate;
 
+import com.opengamma.language.Value;
+import com.opengamma.language.ValueUtils;
 import com.opengamma.language.convert.TypeMap;
 import com.opengamma.language.convert.ValueConversionContext;
 import com.opengamma.language.definition.JavaTypeInfo;
 import com.opengamma.language.invoke.AbstractTypeConverter;
 
 /**
- * Converts dates and times to an R representation. Strings are used.
+ * Converts dates and times to an R representation.
+ * <p>
+ * The following are represented as strings:
+ * <ul>
+ *  <li>LocalDate
+ * </ul>
+ * The following are represented as doubles (seconds since 1970-01-01 epoch):
+ * <ul>
+ *  <li>Instant
+ * </ul>
  */
 public class DateTimeConverter extends AbstractTypeConverter {
 
-  private static final JavaTypeInfo<String> STRING = JavaTypeInfo.builder(String.class).get();
+  private static final JavaTypeInfo<Value> VALUE = JavaTypeInfo.builder(Value.class).get();
+  private static final JavaTypeInfo<Value> VALUE_ALLOW_NULL = JavaTypeInfo.builder(Value.class).allowNull().get();
+  private static final JavaTypeInfo<Instant> INSTANT = JavaTypeInfo.builder(Instant.class).get();
+  private static final JavaTypeInfo<Instant> INSTANT_ALLOW_NULL = JavaTypeInfo.builder(Instant.class).allowNull().get();
   private static final JavaTypeInfo<LocalDate> LOCAL_DATE = JavaTypeInfo.builder(LocalDate.class).get();
-  private static final JavaTypeInfo<String> STRING_ALLOW_NULL = JavaTypeInfo.builder(String.class).allowNull().get();
   private static final JavaTypeInfo<LocalDate> LOCAL_DATE_ALLOW_NULL = JavaTypeInfo.builder(LocalDate.class).allowNull().get();
 
-  private static final TypeMap TO_STRING = TypeMap.of(ZERO_LOSS, LOCAL_DATE);
-  private static final TypeMap FROM_STRING = TypeMap.of(ZERO_LOSS, STRING);
-  private static final TypeMap TO_STRING_ALLOW_NULL = TypeMap.of(ZERO_LOSS, LOCAL_DATE_ALLOW_NULL);
-  private static final TypeMap FROM_STRING_ALLOW_NULL = TypeMap.of(ZERO_LOSS, STRING_ALLOW_NULL);
+  private static final TypeMap TO_VALUE = TypeMap.of(ZERO_LOSS, INSTANT, LOCAL_DATE);
+  private static final TypeMap TO_VALUE_ALLOW_NULL = TypeMap.of(ZERO_LOSS, INSTANT_ALLOW_NULL, LOCAL_DATE_ALLOW_NULL);
+  private static final TypeMap FROM_VALUE = TypeMap.of(MINOR_LOSS, VALUE);
+  private static final TypeMap FROM_VALUE_ALLOW_NULL = TypeMap.of(MINOR_LOSS, VALUE_ALLOW_NULL);
 
   @Override
   public boolean canConvertTo(final JavaTypeInfo<?> targetType) {
     final Class<?> clazz = targetType.getRawClass();
-    if (clazz == String.class) {
+    if (clazz == Value.class) {
       return true;
     } else {
-      for (JavaTypeInfo<?> toData : (targetType.isAllowNull() ? TO_STRING_ALLOW_NULL : TO_STRING).keySet()) {
+      for (JavaTypeInfo<?> toData : (targetType.isAllowNull() ? TO_VALUE_ALLOW_NULL : TO_VALUE).keySet()) {
         if (clazz == toData.getRawClass()) {
           return true;
         }
@@ -53,28 +68,40 @@ public class DateTimeConverter extends AbstractTypeConverter {
       return;
     }
     final Class<?> clazz = type.getRawClass();
-    if (clazz == String.class) {
+    if (clazz == Value.class) {
       if (value instanceof LocalDate) {
-        conversionContext.setResult(((LocalDate) value).toString());
+        conversionContext.setResult(ValueUtils.of(((LocalDate) value).toString()));
+      } else if (value instanceof Instant) {
+        conversionContext.setResult(ValueUtils.of((double) ((Instant) value).toEpochMillisLong() / 1000d));
+      } else {
+        conversionContext.setFail();
+      }
+    } else if (clazz == LocalDate.class) {
+      final String str = ((Value) value).getStringValue();
+      if (str != null) {
+        conversionContext.setResult(LocalDate.parse(str));
+      } else {
+        conversionContext.setFail();
+      }
+    } else if (clazz == Instant.class) {
+      final Double epochSeconds = ((Value) value).getDoubleValue();
+      if (epochSeconds != null) {
+        conversionContext.setResult(Instant.ofEpochSeconds(epochSeconds.longValue()));
       } else {
         conversionContext.setFail();
       }
     } else {
-      if (clazz == LocalDate.class) {
-        conversionContext.setResult(LocalDate.parse((String) value));
-      } else {
-        conversionContext.setFail();
-      }
+      conversionContext.setFail();
     }
   }
 
   @Override
   public Map<JavaTypeInfo<?>, Integer> getConversionsTo(final JavaTypeInfo<?> targetType) {
     final Class<?> clazz = targetType.getRawClass();
-    if (clazz == String.class) {
-      return targetType.isAllowNull() ? TO_STRING_ALLOW_NULL : TO_STRING;
+    if (clazz == Value.class) {
+      return targetType.isAllowNull() ? TO_VALUE_ALLOW_NULL : TO_VALUE;
     } else {
-      return targetType.isAllowNull() ? FROM_STRING_ALLOW_NULL : FROM_STRING;
+      return targetType.isAllowNull() ? FROM_VALUE_ALLOW_NULL : FROM_VALUE;
     }
   }
 
