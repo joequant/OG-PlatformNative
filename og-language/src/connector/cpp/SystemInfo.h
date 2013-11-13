e4a3673cc74b69e5ce63bf61726f9e2b986fee02
Andrew Griffin <andrew@opengamma.com>
2013-11-13 13:17:28 +0000

    [XLS-566] Multiple field sysinfo query

--- og-language/src/connector/cpp/SystemInfo.h
+++ og-language/src/connector/cpp/SystemInfo.h
@@ -18,12 +18,18 @@
 REQUESTBUILDER_BEGIN (CSystemInfoQuery)
 	REQUESTBUILDER_REQUEST (CLASS_com_opengamma_language_config_SystemInfo)
 private:
-	fudge_i32 m_nField;
+	fudge_i32 m_anField[2];
 public:
 	void SetQueryField (int nField) {
 		m_request.fudgeCountGet = 1;
-		m_request._get = &m_nField;
-		m_nField = nField;
+		m_request._get = m_anField;
+		m_anField[0] = nField;
+	}
+	void SetQueryFields (int nField1, int nField2) {
+		m_request.fudgeCountGet = 2;
+		m_request._get = m_anField;
+		m_anField[0] = nField1;
+		m_anField[1] = nField2;
 	}
 	REQUESTBUILDER_RESPONSE (CLASS_com_opengamma_language_config_SystemInfo)
 REQUESTBUILDER_END
