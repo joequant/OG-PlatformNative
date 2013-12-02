b1ae33efe9ae5c26fe911fe2cdc7d504c8d863c8
Andrew Griffin <andrew@opengamma.com>
2013-12-02 17:29:22 +0000

    [PLAT-1292, PLAT-4630] Solution to 1292 using 4630

--- og-language/src/service/cpp/ErrorFeedback.cpp
+++ og-language/src/service/cpp/ErrorFeedback.cpp
@@ -6,8 +6,63 @@
 
 #include "stdafx.h"
 #include "ErrorFeedback.h"
+#include "Public.h"
 
 LOGGING(com.opengamma.language.service.ErrorFeedback);
+
+static PCTSTR *_Split2 (TCHAR **ppszNext, int *pnCount) {
+	TCHAR *pszLine = _tcstok_s (NULL, TEXT ("\n"), ppszNext);
+	if (pszLine) {
+		int nIndex = (*pnCount)++;
+		PCTSTR *ppszResult = _Split2 (ppszNext, pnCount);
+		if (ppszResult) {
+			ppszResult[nIndex] = pszLine;
+			return ppszResult;
+		} else {
+			return NULL;
+		}
+	} else {
+		return (PCTSTR*)malloc (sizeof (PCTSTR) * *pnCount);
+	}
+}
+
+static PCTSTR *_Split (TCHAR *psz, int *pnCount) {
+	TCHAR *pszNext;
+	TCHAR *pszLine = _tcstok_s (psz, TEXT ("\n"), &pszNext);
+	if (pszLine) {
+		(*pnCount)++;
+		PCTSTR *ppszResult = _Split2 (&pszNext, pnCount);
+		if (ppszResult) {
+			ppszResult[0] = pszLine;
+			return ppszResult;
+		} else {
+			return NULL;
+		}
+	} else {
+		*pnCount = 0;
+		return NULL;
+	}
+}
+
+static void _Report (PCTSTR *ppszLines, int nLines) {
+#ifdef _WIN32
+	LOGINFO (TEXT ("Writing failure to start to Windows event log"));
+	HANDLE hEventLog = RegisterEventSource (NULL, ServiceDefaultServiceName ());
+	if (hEventLog) {
+		if (!ReportEvent (hEventLog, EVENTLOG_ERROR_TYPE, 0, 0, NULL, nLines, 0, ppszLines, NULL)) {
+			LOGERROR (TEXT ("Couldn't write to Windows event log, error ") << GetLastError ());
+		}
+		DeregisterEventSource (hEventLog);
+	} else {
+		LOGERROR (TEXT ("Couldn't open Windows event log, error ") << GetLastError ())
+	}
+#else /* ifdef _WIN32 */
+	int n;
+	for (n = 0; n < nLines; n++) {
+		LOGWARN (TEXT (">> ") << ppszLines[n]);
+	}
+#endif /* ifdef _WIN32 */
+}
 
 /// Creates a new feedback object.
 CErrorFeedback::CErrorFeedback () {
@@ -24,13 +79,11 @@ void CErrorFeedback::Write (const TCHAR *pszMessage) {
 	LOGERROR (TEXT ("A major error occurred"));
 	TCHAR *psz = _tcsdup (pszMessage);
 	if (psz) {
-		// TODO: Implement this as writing to a file in a known location perhaps. The caller will already have
-		// logged suitable messages, so we only need to report at DEBUG level.
-		TCHAR *pszNext;
-		TCHAR *pszLine = _tcstok_s (psz, TEXT ("\n"), &pszNext);
-		while (pszLine) {
-			LOGWARN (TEXT (">> ") << pszLine);
-			pszLine = _tcstok_s (NULL, TEXT ("\n"), &pszNext);
+		int nLines = 0;
+		PCTSTR *ppszLines = _Split (psz, &nLines);
+		if (ppszLines) {
+			_Report (ppszLines, nLines);
+			free(ppszLines);
 		}
 		free (psz);
 	} else {
